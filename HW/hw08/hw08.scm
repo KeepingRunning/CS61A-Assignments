ca3f7c335482ca89d4afa26739ed54b4d88eface
KeepingRunning <1599949878@qq.com>
2026-05-04 18:02:17 +0800

    finish hw08

--- HW/hw08/hw08.scm
+++ HW/hw08/hw08.scm
@@ -1,7 +1,31 @@
-(define (ascending? s) 'YOUR-CODE-HERE)
+(define (ascending? s) 
+    (cond
+    ((or (null? s) (null? (cdr s))) #t)
+    ((> (car s) (car (cdr s))) #f)
+    (else (ascending? (cdr s))))
+)
 
-(define (my-filter pred s) 'YOUR-CODE-HERE)
 
-(define (interleave lst1 lst2) 'YOUR-CODE-HERE)
+(define (my-filter pred s) 
+    (cond
+        ((null? s) '())
+        ((pred (car s)) (cons (car s) (my-filter pred (cdr s))))
+        (else (my-filter pred (cdr s)))
+    )
+)
 
-(define (no-repeats s) 'YOUR-CODE-HERE)
+(define (interleave lst1 lst2) 
+    (cond
+        ((null? lst1) lst2)
+        ((null? lst2) lst1)
+        (else (cons (car lst1)
+                    (interleave lst2 (cdr lst1))))
+    )
+)
+
+(define (no-repeats s) 
+    (cond
+        ((null? s) '())
+        (else (cons (car s)
+            (no-repeats (my-filter (lambda (x) (not (= x (car s)))) (cdr s))))))
+)
